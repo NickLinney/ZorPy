ee95d89399d1fe70617590d29e9d96e3ee3b6f0b
Nick Linney <nrlinney@live.com>
2021-09-19 20:15:08 -0400

    Trimmed test code

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from character import loadCharacter, characterDictionaryAppend, characterGetAll
 # Game function.
 def game():
     print("Game started.")
-    loadCharacter("samplechar")
     new_character = characterDictionaryAppend("samplechar")
-
-    print(new_character["name"])
 
     print("Press Enter to view your abilities...")
     keyPress = input()
